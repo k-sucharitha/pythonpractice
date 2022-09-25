083046bf06c273969efc876fec0a1ae4e0a855d1
k-sucharitha <kakanurusucharitha@gmail.com>
2022-09-25 18:58:26 +0530

    changed coding

--- python_basics/prac_1.py
+++ python_basics/prac_1.py
@@ -46,8 +46,57 @@ The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line
 # print(name1.rstrip('vi')) # 'vivvvnaykumarreddy'
 # print(name1.lstrip('vi')) # 'naykumarreddyiviiii'
 
-## find, format, join
+## find() used to return the lowest index value of the first occurrence of the substring from the input string; else it returns -1. 
 # name = 'vivvvnaykumarreddyiviiii'
-# print(name.find())
+# print(name.find('iv'))
 
 
+## format() is a technique of the string category permits you to try and do variable substitutions and data formatting.
+# string = '{} is a good option for beginners in python'
+# v = [1, 2, 3] # int/str/list/tuple/dict any type can be replaced in the {} while using format()
+# print(string.format(v))
+
+# string = '{} is a good option for {} in python'
+# v = [1, 2, 3] # int/str/list/tuple/dict any type can be replaced in the {} while using format()
+# print(string.format(v)) # error must pass two args to replace two {} in string
+# print(string.format(v,v))
+
+## positional vs named args
+# --------------------------
+# 1) positional formatting
+# string = '{} is a good option for {} in python'
+# a = "dash_1"
+# b = "dash_2"
+# print(string.format(a,b)) # dash_1 is a good option for dash_2 in python
+# print(string.format(b,a)) # dash_2 is a good option for dash_1 in python
+
+# 2) named formatting
+# string = '{x1} is a good option for {x2} in python'
+# a = "dash_1"
+# b = "dash_2"
+# print(string.format(x1="vinay",x2="hdsbhdsbs")) # vinay is a good option for hdsbhdsbs in python
+# print(string.format(x2=a,x1=b)) # dash_2 is a good option for dash_1 in python
+
+# suchi_details = {"name":"Suchi", "age":23}
+# vinay_details = {"name":"Vinay", "age":27}
+# bharathi_details = {"name":"Bharathi", "age":51}
+
+# # print("Hi, MY name is Suchi I am 23 years old") # static print (values are hard-coded)
+# # dynamic print (using string format())
+# print("Hi, MY name is {name} I am {age} years old".format(name=bharathi_details['name'], age=bharathi_details['age'])) 
+
+# join() is an inbuilt string function in Python used to join elements of the sequence separated by a string separator
+# list = ["1","2","3","4"] # an iterable object
+# string = "vinaykumar" # an iterable object
+# tuple = ("vinay", "kumar", ) # an iterable object
+# dict = {"mobile":"one plus", "price":"40,999"} # an iterable object
+# object = 23 # not an iterable object
+
+# print(" --> ".join(list)) # 1 --> 2 --> 3 --> 4
+# print(" --> ".join(string)) # v --> i --> n --> a --> y --> k --> u --> m --> a --> r
+# print(" --> ".join(tuple)) # vinay --> kumar
+# print(" --> ".join(dict)) # mobile --> price
+# print(" --> ".join(object)) # TypeError: can only join an iterable
+
+########################################################################################
+########################################################################################
