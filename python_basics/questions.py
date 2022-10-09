211517e4900f602e55b344b0929be7af2fcfff85
k-sucharitha <kakanurusucharitha@gmail.com>
2022-10-09 22:00:08 +0530

    added questions file for test practice

---
+++ python_basics/questions.py
@@ -0,0 +1,33 @@
+"""
+1) Prepare a list of even numbers start from 2 to 20.
+
+a=[]
+for i in range(2,20,2):
+    a.append(i)
+print(a)
+
+2) Find the numbers which are divisible by 5 from the above prepared list
+
+for i in a:
+    if i%5==0:
+        print('given number divisible by 5 is:',i)
+
+
+3) "I am learning Python" Replace this string to "I am learning git" 
+and add this to the above list at the middle position/index of the list
+
+v="I am learning Python"
+k=v.replace('Python','git')    
+print(k)
+
+mid_pos = len(a)//2
+a[mid_pos]=k
+print(a)
+
+4) Print the index and value from the above list. Output should be in the below format 
+--> index of value X is Y (where X is the element and Y is index)
+
+for idx, val in enumerate(a):
+    print(f"index of element {val} is {idx}")
+
+"""
