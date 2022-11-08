c293ebcac6659370265d5b3c6fecffe936097697
k-sucharitha <kakanurusucharitha@gmail.com>
2022-11-08 15:20:14 +0530

    reading csv

--- pandas_practice/countries.py
+++ pandas_practice/countries.py
@@ -5,4 +5,10 @@ import pandas as pd
 df = pd.read_csv("E:\Suchi\GIT_Repos\pythonpractice\data_files\country_zones.csv")
 
 # print(len(df['Country_Name']))
-print(len(list(set(df['Country_Name']))))
+country_names=list(set(df['Country_Name']))
+# print(country_names)
+
+
+for i in country_names:
+    if i[0]=='I':
+        print(i)
