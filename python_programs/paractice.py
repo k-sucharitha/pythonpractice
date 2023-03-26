461d92c96b793ee6893e41f9c503027b3bbd81cf
k-sucharitha <kakanurusucharitha@gmail.com>
2023-03-26 20:49:10 +0530

    added new files

---
+++ python_programs/paractice.py
@@ -0,0 +1,7 @@
+from postgresl_connection import get_db_connection
+
+get_db_connection()
+print(f"My name is:{get_db_connection}")
+
+
+ 
---
+++ python_programs/quiz_game.py
@@ -0,0 +1,45 @@
+print("Welcome to my computer quiz!")
+
+playing = input("Do you want to play?")
+
+if playing .lower()!="yes":
+    quit()
+
+print("okay! Let's play:)")
+score =0
+answer= input("What does CPU stand for? ")
+
+if answer.lower()== "central processing unit":
+    print('Correct!')
+    score +=1
+    
+else:
+    print("Your answer is wrong")
+
+answer = input("What does GPU stand for? ")
+if answer.lower()== "graphics processing unit":
+    print('Correct!')
+    score +=1
+else:
+    print("Your answer is wrong")
+
+answer = input("What does RAM stand for? ")
+if answer.lower()== "random access memory":
+    print('Correct!')
+    score +=1
+else:
+    print("Your answer is wrong")
+
+answer = input("What does PSU stand for? ")
+if answer.lower()== "power supply":
+    print('Correct!')
+    score +=1
+else:
+    print("Your answer is wrong")
+    
+print("you got"+str(score)+ "questions correct!")  
+print("you got"+str((score/4)*100)+ "%.")
+
+
+
+   
