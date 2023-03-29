cf85c5c28a18e144b83e29e4b2d04ad4d0549a77
k-sucharitha <kakanurusucharitha@gmail.com>
2023-03-29 19:29:12 +0530

    rock_paper_scissors project added

--- python_programs/number_guesser.py
+++ python_programs/number_guesser.py
@@ -1,3 +1,16 @@
+""" 
+Guess the Number between 0 and top_of_range which is given by User. 
+
+Step1: Collect the user input for top_of_range
+Step2: Pick a random number from 0 to top_of_range which
+Step3: Start a loop and make guesses to find the number
+Step4: If your guessed number and random number is matched you won andstop the loop 
+Step5: If it is greater, then give a hint to the user to guess lower number
+Step6: If it is lower, then give a hint to the user to guess greater number
+
+At the end print how many guesses an user has taken to guess the number correctly.
+"""
+
 import random
 
 # input() always returns string datatype "10"
--- python_programs/quiz_game.py
+++ python_programs/quiz_game.py
@@ -2,17 +2,16 @@ print("Welcome to my computer quiz!")
 
 playing = input("Do you want to play?")
 
-if playing .lower() != "yes":
+if playing.lower() != "yes":
     quit()
 
 print("okay! Let's play:)")
 score = 0
-answer = input("What does CPU stand for? ")
 
+answer = input("What does CPU stand for? ")
 if answer.lower() == "central processing unit":
     print('Correct!')
     score += 1
-
 else:
     print("Your answer is wrong")
 
@@ -37,5 +36,5 @@ if answer.lower() == "power supply":
 else:
     print("Your answer is wrong")
 
-print("you got"+str(score) + "questions correct!")
-print("you got"+str((score/4)*100) + "%.")
+print("you got " + str(score) + " questions correct!")
+print("you got " + str((score/4)*100) + "%.")
