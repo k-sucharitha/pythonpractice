eef8709c8d1a33abb5f491a7c38ba90d9a8bea78
k-sucharitha <kakanurusucharitha@gmail.com>
2023-05-03 10:45:26 +0530

    added new file

--- python_programs/email_send.py
+++ python_programs/email_send.py
@@ -1,11 +1,10 @@
 import smtplib
-from cred import my_mail,my_pwd
 
 s = smtplib.SMTP('smtp.gmail.com', 587)
 s.starttls()
-s.login ("kakanurusucharitha@gmail.com", my_pwd)
+s.login("my_email", "my_pwd")
 msg = "please subscribe to my channel"
-reciever_mail ="kvinaykumarreddy1995@gmail.com"
-s.send_mail(my_mail,reciever_mail,msg)
+reciever_mail = "kvinaykumarreddy1995@gmail.com"
+s.send_mail("my_email", reciever_mail, msg)
 s.quit()
 print("successsfully sent")
